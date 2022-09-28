bb50347e6ce439459f0171d9cbd04fe816155e79
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2022-09-28 11:34:47 +0330

    rename user to person and create repository and controller

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -7,10 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity(name="BOOK")
+@Entity(name="PERSON")
 @Data
 @NoArgsConstructor
-public class User {
+public class Person {
 
     @Id
     @GeneratedValue
